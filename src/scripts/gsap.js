aff49ecaea6742dc52565706a4cc14f3bb2561ad
nowakkamil <nowakkamil@yahoo.com>
2020-10-21 01:06:04 +0200

    Compose a draft version of landing and experience sections

--- src/scripts/gsap.js
+++ src/scripts/gsap.js
@@ -1,7 +1,25 @@
-import { TweenMax, Power3 } from "gsap/all";
+import { gsap, TimelineMax, Power3 } from "gsap/all";
 
-TweenMax.to(".intro", 2, {
-    scrollTrigger: ".intro",
-    opacity: ".8",
-    ease: Power3.easeOut
-});
+const animateMarks = () => {
+	let marks = document.querySelectorAll(".landing-section__mark-icon");
+	let mark = document.querySelector(".landing-section__mark-icon");
+	let tl = new TimelineMax();
+
+	gsap.to(".landing-section__mark-icon", {
+		duration: 0.5,
+		ease: "none",
+		x: "+=100%", // move each box 500px to right
+		modifiers: {
+			x: gsap.utils.unitize(x => {
+				return parseFloat(x) % 100;
+			}, "%") // force x value to be between 0 and 500 using modulus
+		},
+		repeat: -1
+	});
+
+	// setTimeout() is crucial
+};
+
+export {
+	animateMarks
+};
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,2 +1,53 @@
 import '../styles/index.scss';
-import '../scripts/gsap';
+import { animateMarks } from '../scripts/gsap';
+import '@iconfu/svg-inject';
+import { CSSPlugin } from "gsap/all";
+
+// Prevent the webpack from performing tree shaking
+const plugin = CSSPlugin;
+
+const getSvgNode = () => {
+    let svg = document.createElement('img');
+    return Object.assign(svg, {
+        className: 'landing-section__mark-icon',
+        src: 'public/icon-code.svg',
+        onload: () => SVGInject(svg)
+    });
+};
+
+let svgNodes = document.createDocumentFragment();
+
+for (let i = 0; i < 30; i++) {
+    let svgNode = getSvgNode();
+    svgNodes.appendChild(svgNode);
+}
+
+document.querySelector(".landing-section__mark")
+    .appendChild(svgNodes);
+
+window.mutationObserver = new MutationObserver(function (mutations, observer) {
+    let i = 0;
+
+    mutations.forEach(function (mutation) {
+        console.log(mutation);
+        i++;
+    });
+
+    console.log(i);
+
+    if (i === 30) {
+        animateMarks();
+        delete window.mutationObserver;
+        observer.disconnect();
+        stop(); // TODO: Verify if needed
+    }
+});
+
+mutationObserver.observe(document.querySelector(".landing-section__mark"), { subtree: true, childList: true });
+
+const stop = () => mutationObserver.disconnect();
+
+setTimeout(() => {
+    mutationObserver.disconnect();
+    console.log(mutationObserver);
+}, 4000);
